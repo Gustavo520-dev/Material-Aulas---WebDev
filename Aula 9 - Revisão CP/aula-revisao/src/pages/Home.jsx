3cc53e68e0bd2343e3154337d47378be4a1f0498
Gustavo Cavalcanti <gucavalcanti2018@gmail.com>
2025-10-09 22:33:21 -0300

    aaa

--- Aula 9 - Revisão CP/aula-revisao/src/pages/Home.jsx
+++ Aula 9 - Revisão CP/aula-revisao/src/pages/Home.jsx
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
+
+  const [reactRepos, setReactRepos] = useState([]);
+  const API = import.meta.env.VITE_GITHUB_API;
+  
+  useEffect(() => {
+    fetch(`${API}node&per_page=5`)
+    .then(res => res.json())
+    .then(data => setReactRepos(data.items)) 
+  }, [])
   
   return (
     <div>
